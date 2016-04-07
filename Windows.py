af86a49071a5e554430a73b3a80442ee9d6589ec
bozhanov1512 <bozhanov1512@students.d211.org>
2016-04-07 11:56:27 -0500

    Commenting.001

--- Windows.py
+++ Windows.py
@@ -1,10 +1,9 @@
-
 import win32api as API
 import win32gui as UI
 import win32 as WIN
 import win32con as con
 import win32file as WF
-#import win32ui as U
+# import win32ui as U
 import win32com as COM
 import winerror
 from ctypes import *
@@ -13,6 +12,7 @@ import File as file
 
 import sys
 
+
 # Globals
 
 
@@ -20,6 +20,14 @@ import sys
 
 
 # Globals END
+
+'''
+Description - Loads the specified bitmap file into memory adn returns a handle to the bitmap.
+
+:param hDC - Handle to the current Device Context.
+:param fileName - A string specifying the path to the bitmap file to be loaded.
+:returns - A handle to the loaded bitmap.
+'''
 
 def loadImgBMP(hDC, fileName):
     if not file.fileExists(fileName):
@@ -36,8 +44,17 @@ def loadImgBMP(hDC, fileName):
 
     return hbmp
 
-def loadBMPIntoDC(hDC, hbmp, x, y):
+'''
+Description - Draws a bitmap into the Device Context specified at the given coordinates.
 
+:param hDC - Handle to the Device Context the bitmap will be drawn to.
+:param hbmp - A handle to the bitmap
+:param x - The x coordinate.
+:param y - The y coordinate
+:returns True if the function succeeded, otherwise False.
+'''
+
+def loadBMPIntoDC(hDC, hbmp, x, y):
     memDC = UI.CreateCompatibleDC(hDC)
     if not memDC:
         return False
@@ -55,10 +72,16 @@ def loadBMPIntoDC(hDC, hbmp, x, y):
     UI.DeleteDC(memDC)
     return True
 
+'''
+Description - Returns the dimensions of the specified bitmap.
+'''
+
 def getBmpDimensions(hbmp):
     bm = UI.GetObject(hbmp)
     ret = [bm.bmWidth, bm.bmHeight]
     return ret
+
+# Draws a circle to the specified DC at (x, y) with diameter of diam.
 
 def drawCircle(hdc, x, y, diam):
     radius = int(diam / 2)
@@ -68,8 +91,15 @@ def drawCircle(hdc, x, y, diam):
     brY = y + radius
     UI.Ellipse(hdc, tlX, tlY, brX, brY)
 
+# Creates an input box at (x, y) with dimensions width and height, with an the id specified which will be used as a notification code in the WndProc.
+# hWnd is a handle to the window on which the input box is to be drawn on, an hInstance a handle to the instance of the program.
+
 def createInputBox(x, y, width, height, id, hWnd, hInstance):
-    return UI.CreateWindow("EDIT", None, con.WS_CHILD | con.WS_BORDER | con.WS_VISIBLE | con.ES_LEFT | con.ES_MULTILINE, x, y, width, height, hWnd, id, hInstance, None)
+    return UI.CreateWindow("EDIT", None, con.WS_CHILD | con.WS_BORDER | con.WS_VISIBLE | con.ES_LEFT | con.ES_MULTILINE,
+                           x, y, width, height, hWnd, id, hInstance, None)
+
+# Creates an output box at (x, y) with dimensions width and height, with an the id specified which will be used as a notification code in the WndProc.
+# hWnd is a handle to the window on which the input box is to be drawn on, an hInstance a handle to the instance of the program.
 
 def createOutputBox(x, y, width, height, id, hWnd, hInstance):
     hOWnd = createInputBox(x, y, width, height, id, hWnd, hInstance)
@@ -77,8 +107,15 @@ def createOutputBox(x, y, width, height, id, hWnd, hInstance):
     UI.SetWindowLong(hOWnd, con.GWL_STYLE, hWWndStyles | con.WS_DISABLED)
     return hOWnd
 
+# Creates a button at (x, y) with dimensions width and height, with an the id specified which will be used as a notification code in the WndProc.
+# hWnd is a handle to the window on which the input box is to be drawn on, an hInstance a handle to the instance of the program.
+
 def createButton(text, x, y, width, height, id, hWnd, hInstance):
-    return UI.CreateWindow("BUTTON", text, con.WS_CHILD | con.WS_VISIBLE | con.BS_FLAT | con.BS_CENTER | con.BS_PUSHBUTTON, x, y, width, height, hWnd, id, hInstance, None)
+    return UI.CreateWindow("BUTTON", text,
+                           con.WS_CHILD | con.WS_VISIBLE | con.BS_FLAT | con.BS_CENTER | con.BS_PUSHBUTTON, x, y, width,
+                           height, hWnd, id, hInstance, None)
+
+# Returns a handle to a font with the specified size, and weight. pPerInch specifies the pixels per inch of the screen.
 
 def createFont(fontSize, pPerInchY, weight, isItalic, isUnderlined, isCrossedOut):
     logFont = UI.LOGFONT()
@@ -100,12 +137,12 @@ def createFont(fontSize, pPerInchY, weight, isItalic, isUnderlined, isCrossedOut
     hFont = UI.CreateFontIndirect(logFont)
     return hFont
 
+
 # wndclass - Initializes and stores all of the information regarding the WNDCLASS data structure and
 # maneges the creation of a window.
 
 class WNDClass:
-
-    def __init__(self, wndProc):                                                         # constructor definition
+    def __init__(self, wndProc):  # constructor definition
         self.className = "WNDClass"
         self.hInstance = UI.GetModuleHandle(None)
         self.dwStyle = con.WS_OVERLAPPEDWINDOW
@@ -115,13 +152,12 @@ class WNDClass:
         self.wndClass.style = con.CS_VREDRAW | con.CS_HREDRAW
         self.wndClass.hInstance = self.hInstance
         self.wndClass.lpfnWndProc = wndProc
-        self.wndClass.hIcon = UI.LoadIcon(0, con.IDI_APPLICATION)               # Default application icon
-        self.wndClass.hCursor = UI.LoadCursor(0, con.IDC_ARROW)                 # normal mose pointer as cursor
-        self.wndClass.hbrBackground = UI.GetStockObject(con.WHITE_BRUSH)        # white background brush
-        self.wndClass.lpszClassName = self.className                            # class name doesn't work in python...
+        self.wndClass.hIcon = UI.LoadIcon(0, con.IDI_APPLICATION)  # Default application icon
+        self.wndClass.hCursor = UI.LoadCursor(0, con.IDC_ARROW)  # normal mose pointer as cursor
+        self.wndClass.hbrBackground = UI.GetStockObject(con.WHITE_BRUSH)  # white background brush
+        self.wndClass.lpszClassName = self.className  # class name doesn't work in python...
 
-        self.wndClassAtom = UI.RegisterClass(self.wndClass)                     # ...so the ATOM must be used
-
+        self.wndClassAtom = UI.RegisterClass(self.wndClass)  # ...so the ATOM must be used
 
     def setResizable(self, isResizable):
         if not isResizable:
@@ -134,9 +170,8 @@ class WNDClass:
         # 4 - 7th parameter:
         # horizontal, vertical, x, y default positions and dimensions
 
-        UI.ShowWindow(self.hwnd, con.SW_SHOWNORMAL)           # send the first WM_PAIN msg
+        UI.ShowWindow(self.hwnd, con.SW_SHOWNORMAL)  # send the first WM_PAIN msg
         UI.UpdateWindow(self.hwnd)
 
-
     def start(self):
-        UI.PumpMessages()                                     # this is actually a loop
+        UI.PumpMessages()  # this is actually a loop
